979ae8028992dc703fdf287e16551ecef7317055
darlal <darlal@users.noreply.github.com>
2021-04-21 15:24:15 -0400

    Enable sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,19 @@
-import typescript from '@rollup/plugin-typescript'
+import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+const isProd = process.env.BUILD === 'production';
 
 export default {
   input: 'src/main.ts',
   treeshake: false,
   output: {
     dir: 'dist/darlal-switcher-plus',
-    sourcemap: false,
+    sourcemap: 'inline',
+    sourcemapExcludeSources: isProd,
     format: 'cjs',
     exports: 'default',
   },
   external: ['obsidian'],
-  plugins: [
-    // typescript(),
-    // typescript({ sourceMap: true }),
-    typescript({ sourceMap: true, inlineSources: true }),
-    // typescript({ inlineSourceMap: true, inlineSources: true }),
-    // typescript({ sourceMap: true, inlineSourceMap: true, inlineSources: true }),
-    nodeResolve(),
-    commonjs(),
-  ],
+  plugins: [typescript(), nodeResolve(), commonjs()],
 };
