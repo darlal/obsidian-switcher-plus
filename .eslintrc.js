b05687b2a5ff73fb45925721ec7ad31ce307acc0
darlal <darlal@users.noreply.github.com>
2021-08-05 14:24:15 -0400

    Configure required type checking to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,16 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
+  ignorePatterns: ['*.config.js', '.eslintrc.js', '.prettierrc.js'],
   plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:jest/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
   ],
   rules: {
