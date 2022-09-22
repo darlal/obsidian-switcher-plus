5e2d1a361c0807a20fd86d61807df6b3356d5434
darlal <darlal@users.noreply.github.com>
2022-09-21 21:20:37 -0400

    feat: add icon for each global command

--- src/main.ts
+++ src/main.ts
@@ -18,44 +18,51 @@ export default class SwitcherPlusPlugin extends Plugin {
       'switcher-plus:open-editors',
       'Open in Editor Mode',
       Mode.EditorList,
+      'lucide-file-edit',
     );
     this.registerCommand(
       'switcher-plus:open-symbols',
       'Open in Symbol Mode',
       Mode.SymbolList,
+      'lucide-dollar-sign',
     );
     this.registerCommand(
       'switcher-plus:open-workspaces',
       'Open in Workspaces Mode',
       Mode.WorkspaceList,
+      'lucide-album',
     );
     this.registerCommand(
       'switcher-plus:open-headings',
       'Open in Headings Mode',
       Mode.HeadingsList,
+      'lucide-file-search',
     );
     this.registerCommand(
       'switcher-plus:open-starred',
       'Open in Starred Mode',
       Mode.StarredList,
+      'star',
     );
     this.registerCommand(
       'switcher-plus:open-commands',
       'Open in Commands Mode',
       Mode.CommandList,
+      'run-command',
     );
-
     this.registerCommand(
       'switcher-plus:open-related-items',
       'Open in Related Items Mode',
       Mode.RelatedItemsList,
+      'lucide-file-plus-2',
     );
   }
 
-  registerCommand(id: string, name: string, mode: Mode): void {
+  registerCommand(id: string, name: string, mode: Mode, iconId?: string): void {
     this.addCommand({
       id,
       name,
+      icon: iconId,
       hotkeys: [],
       checkCallback: (checking) => {
         // modal needs to be created dynamically (same as system switcher)
