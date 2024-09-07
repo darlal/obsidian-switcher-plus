16d17c4bf135e8b0b25a7345a816b7b8cc6cb8c1
darlal <darlal@users.noreply.github.com>
2024-09-07 12:33:06 -0400

    build: compress and strip comments in production

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,32 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 const isProd = process.env.BUILD === 'production';
+const tsconfig = isProd ? './tsconfig.prod.json' : './tsconfig.json'
 
 export default {
   input: 'src/main.ts',
   treeshake: false,
   output: {
     dir: 'dist/darlal-switcher-plus',
-    sourcemap: 'inline',
+    sourcemap: isProd ? false : 'inline',
     sourcemapExcludeSources: isProd,
     format: 'cjs',
     exports: 'default',
   },
   external: ['obsidian', 'electron'],
-  plugins: [typescript(), nodeResolve(), commonjs()],
+  plugins: [
+    typescript({ tsconfig }),
+    nodeResolve(),
+    commonjs(),
+    isProd && terser({
+          format: {
+            comments: false
+          },
+          sourceMap: false,
+          compress: true
+        })
+  ],
 };
